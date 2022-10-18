d2b10004f0a0bbbd4bc7299175c6e0e169dcccc5
askaf-11 <115477579+askaf-11@users.noreply.github.com>
2022-10-18 10:29:12 +0530

    React Js Test

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var txt = /*#__PURE__*/React.createElement("div", null, " ", /*#__PURE__*/React.createElement("h1", null, "Hello React"), /*#__PURE__*/React.createElement("p", null, "This is paragraph"));
+ReactDOM.render(txt, document.getElementById('root'));
+
+var txt2 = /*#__PURE__*/React.createElement("footer", {
+    className: "footer"
+  }, /*#__PURE__*/React.createElement("span", {
+    className: "footer-text"
+  }, "Design By Askaf"));
+ReactDOM.render(txt2, document.getElementById('footer'));
